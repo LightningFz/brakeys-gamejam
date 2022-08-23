559b99ccd029107915f2d99f350e58697711ca68
LightningFz <96966812+LightningFz@users.noreply.github.com>
2022-08-22 22:21:16 -0400

    shooting

---
+++ brakeys-gamejam/Assets/scripts/shooting.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+
+    public GameObject bulletPrefab; 
+    public Transform firepoint;
+
+    public float bulletForce = 20f;
+    
+    public float CameraShakePower;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firepoint.up * bulletForce, ForceMode2D.Impulse);
+        //CinemachineShake.Instance.ShakeCamera(CameraShakePower, 0.1f);
+    }
+}
