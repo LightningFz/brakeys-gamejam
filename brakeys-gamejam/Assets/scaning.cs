c32176ccb95147c66340698d26c248d05e5a327e
LightningFz <96966812+LightningFz@users.noreply.github.com>
2022-08-24 15:43:13 -0400

    A*
    it all works fine, but the graph is scanning before spawning the obstacles

---
+++ brakeys-gamejam/Assets/scaning.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using a = AstarPath;
+
+public class scaning : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //AstarPath.active.data.recastGraph.scanEmptyGraph = true;
+       //AstarPath.active.Scan();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
