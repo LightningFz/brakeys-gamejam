d9288b9b726766c2b8d275574f803eaa21089c3a
LightningFz <96966812+LightningFz@users.noreply.github.com>
2022-08-27 02:19:58 -0400

    enemy healthbar

--- brakeys-gamejam/Assets/scripts/enemyHealth.cs
+++ brakeys-gamejam/Assets/scripts/enemyHealth.cs
@@ -8,15 +8,28 @@ public class enemyHealth : MonoBehaviour
     public healthbar healthBar;
     public int maxHealth = 50;
 
+    public RectTransform healthBarRect;
+
+    private Quaternion rotation;
+    private Vector3 position;
+    /*void Awake()
+    {
+        //rotation = transform.rotation;
+        //position = transform.parent.position - transform.position;
+    }*/
     // Start is called before the first frame update
     void Start()
     {
         healthS = new healthSystem(maxHealth);
+        healthBar.setMaxHealth(healthS.healthMax);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //transform.rotation = rotation;
+        //transform.position = transform.parent.position - position;
+        healthBar.setHealth(healthS.health);
         Die();
     }
     public void Die()
